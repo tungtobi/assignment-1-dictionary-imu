1eb5d5579ff59fba843138d92abd2402aac14a5a
Do Hong Phong <phong987phong@gmail.com>
2018-09-25 12:47:51 +0700

    Ver4

---
+++ ver4/src/edu/uet/imu/dictIMU/Main.java
@@ -0,0 +1,10 @@
+package edu.uet.imu.dictIMU;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        DictionaryApplication application = new DictionaryApplication();
+        application.runApplication(args);
+    }
+}
---
+++ ver4/src/edu/uet/imu/dictIMU/controller/Controller.java
@@ -0,0 +1,32 @@
+package edu.uet.imu.dictIMU.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+import javafx.collections.transformation.FilteredList;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+
+	public void handleSearch(ActionEvent actionEvent)
+	{
+	}
+
+	public void handleSearchField(ActionEvent actionEvent)
+	{
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle)
+	{
+	}
+
+}
