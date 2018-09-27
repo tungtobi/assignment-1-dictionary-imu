f797a220baf5fe403060a10929013fd3331033a6
Do Hong Phong <phong987phong@gmail.com>
2018-09-27 13:49:07 +0700

    Ver5 - Search from web

---
+++ ver5/src/edu/uet/imu/dictIMU/DictionaryCommandLine.java
@@ -0,0 +1,95 @@
+package edu.uet.imu.dictIMU;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DictionaryCommandLine
+{
+	private DictionaryCommandline dictionaryCommandline;
+	private DictionaryManagement dictionaryManager;
+    private Dictionary dictionary;
+
+	public DictionaryCommandLine()
+	{
+		dictionaryCommandline = new DictionaryCommandline();
+		dictionaryManager = new DictionaryManagement();
+        dictionary = dictionaryManager.getDictionary();
+	}
+
+	public void dictionaryBasic()
+	{
+		Scanner scanner = new Scanner(System.in);
+		char c = ' ';
+		do 
+		{
+			System.out.println("1. Thêm từ mới");
+			System.out.println("2. Hiện tất cả các từ");
+			System.out.println("0. Thoát");
+			System.out.println("=========================");
+			System.out.print("Lựa chọn: ");
+			c = scanner.next().charAt(0);
+		  	switch (c)
+		  	{
+		  		case '1':
+					dictionaryManager.insertFromCommandline();
+		  			break;
+		 		case '2':
+					dictionaryCommandline.showAllWords(dictionary.getWords());
+					break;
+			}
+			System.out.println("=========================");
+		}
+		while (c != '0');
+	}
+
+    public void dictionaryAdvenced()
+    {
+        Scanner scanner = new Scanner(System.in);
+		char c = ' ';
+		do 
+		{
+			System.out.println("1. Thêm từ mới từ File");
+			System.out.println("2. Hiện tất cả các từ");
+            System.out.println("3. Tìm kiếm từ");
+            System.out.println("4. Xóa từ");
+            System.out.println("5. Xuất từ điển ra file");
+			System.out.println("0. Thoát");
+			System.out.println("=========================");
+			System.out.print("Lựa chọn: ");
+			c = scanner.next().charAt(0);
+		  	switch (c)
+		  	{
+		  		case '1':
+					dictionaryManager.insertFromFile("out/data/dictionary.txt");
+		  			break;
+		 		case '2':
+					dictionaryCommandline.showAllWords(dictionaryManager.getDictionary().getWords());
+					break;
+                case '3':
+                    System.out.print("Nhập từ cần tra: ");
+                    String searchWord = scanner.next();
+                    ArrayList<Word> resultWords = DictionarySearcher.searcherForCommandline(searchWord.toLowerCase(), dictionary.getWords());
+                    if (resultWords == null)
+                        System.out.println("Không tìm thấy từ cần tra");
+                    else 
+                        dictionaryCommandline.showAllWords(resultWords);
+                    break;
+                case '4':
+                    dictionaryManager.removeFromCommandline();
+                    break;
+                case '5':
+                    dictionaryManager.dictionaryExportToFile("out/data/out.txt");
+			}
+			System.out.println("=========================");
+		}
+		while (c != '0');
+
+    }
+
+
+	public static void runCommandline(String[] args)
+	{
+		DictionaryCommandLine dictionary = new DictionaryCommandLine();
+		dictionary.dictionaryAdvenced();
+	}
+}
